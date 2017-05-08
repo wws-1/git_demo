a6ba93426feee6ceaea718264e61e63504cd59c9
wws-1 <vip@192.168.7.74>
2017-05-08 11:47:10 +0800

    首次提交到本地仓库

---
+++ src/git_demo/Helloworld.java
@@ -0,0 +1,19 @@
+package git_demo;
+
+public class Helloworld {
+
+	/**
+
+	    * Êä³öÒ»ÐÐ×Ö·û´®¡°Hello World!¡±
+
+	    * @param args
+
+	    */
+
+	    public static void main(String[] args) {
+
+	    System.out.println("Hello World!");
+
+	    }
+}
+
